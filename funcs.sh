d3d117f8fd458891397fe4f553cb203036f30c2c
Young <young@4ns.me>
2024-03-04 19:40:35 +0900

    support two more sites

--- funcs.sh
+++ funcs.sh
@@ -1,9 +1,57 @@
 
 download_to() {
+	local N=${3:-1}
+
     if ! command -v yt-dlp &> /dev/null; then
         ffmpeg -hide_banner -loglevel info -i "$1" -c copy $2 < /dev/null;
     else
-        yt-dlp -N 1 -o "$2" "$1"
+        # yt-dlp -r 1M -N $N --retry-sleep 10 -o "$2" --use-postprocessor FFmpegCopyStream --ppa CopyStream:"-c:v libx264 -c:a aac -f mp4" "$1"
+        yt-dlp -r 1M -N $N --retry-sleep 10 -o "$2" "$1"
     fi
 }
 
+get_url_root() {
+    echo "$1" | grep -oP "https?://[.0-9a-z]+"
+}
+
+olevod_check_url() {
+    local url="$1"
+    local id
+    local sid
+    local nid
+    if [[ $url == *"olevod.com"* ]]; then
+        # https://www.olevod.com/index.php/vod/detail/id/11111.html
+        # https://www.olevod.com/index.php/vod/play/id/11111/sid/1/nid/1.html
+        id=$(echo "$url" | grep -oP '(?<=/id/)[^./]+')
+        sid=$(echo "$url" | grep -oP '(?<=/sid/)[^./]+')
+        nid=$(echo "$url" | grep -oP '(?<=/nid/)[^./]+')
+        if [[ -z $sid ]]; then
+            echo "https://www.olevod.com/index.php/vod/play/id/$id/sid/1/nid/1.html"
+        else
+            echo "$url"
+        fi
+    elif [[ $url == *"olevod.app"* ]]; then
+        # https://www.olevod.app/playlist_list=/123ABC.html
+        # https://www.olevod.app/watch_v=123ABC-1-1.html
+        id=$(echo "$url" | grep -oP '(?<=/playlist_list=/)[^.]+')
+        if [[ -z $id ]]; then
+            echo "$url"
+        else
+            echo "https://www.olevod.app/watch_v=$id-1-1.html"
+        fi
+    elif [[ $url == *"olevod.one"* ]]; then
+        # https://www.olevod.one/vod/201011111
+        # https://www.olevod.one/vod/201011111/play/ep1
+        # https://www.olevod.one/_olevod_lazy/201011111-ep1'
+        id=$(echo "$url" | grep -oP '(?<=vod/)[^/]+')
+        nid=$(echo "$url" | grep -oP "(?<=/play/ep)[^/]+")
+        if [[ -z $nid ]]; then
+            echo "https://www.olevod.one/vod/$id/play/ep1"
+        else
+            echo "$url"
+        fi
+    else
+        echo -e "\033[0;31mInvalid URL.\033[0m" >&2
+        exit 1
+    fi
+}
--- olevod_extractor.sh
+++ olevod_extractor.sh
@@ -11,7 +11,7 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-next=$1
+next=$(olevod_check_url $1)
 target=$2
 download=$3
 if [ -z "$3" ]; then
@@ -25,28 +25,51 @@ if [ -z "$3" ]; then
 else
 	target=${2:-output}
 	target=${target%/}
-	if [ -e "$target" ] && [ ! -d "$target" ]; then
+	if [ ! -e "$target" ]; then
+		mkdir -p "$target"
+	elif [ -e "$target" ] && [ ! -d "$target" ]; then
 		echo -e "\033[0;31mError: $target exists and is not a folder.\033[0m" >&2
 		exit 1
 	fi
 fi
 
-root_url='https://www.olevod.com'
+root_url=$(get_url_root $next)
 last_url=$root_url
 
 extract_from() {
     TMPHTML=tmp.html
     TMPJSON=tmp.json
+    local id
+    local nid
+    local m3u8
 
-    curl "$url" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' -H 'Accept-Language: ja-JP,ja;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-US;q=0.6,en;q=0.5' -H 'Cache-Control: max-age=0'  -H 'Connection: keep-alive' -H 'DNT: 1' -H "Referer: $last_url" -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -c olevod.cookies -o $TMPHTML
+    if [[ $url == *"olevod.one"* ]]; then
+        id=$(echo "$url" | grep -oP '(?<=vod/)[^/]+')
+        nid=$(echo "$url" | grep -oP "(?<=/play/ep)[^/]+")
+        local apiurl="https://www.olevod.one/_olevod_lazy/$id-ep$nid"
 
-    grep -oP 'player_aaaa=\K[^<]+' $TMPHTML > $TMPJSON
+        curl --fail-with-body "$apiurl" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: ja-JP,ja;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-US;q=0.6,en;q=0.5' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Host: www.olevod.one' -H 'Referer: $last_url' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' -H 'sec-ch-ua: "Chromium";v="114", "Google Chrome";v="114", ";Not A Brand";v="99"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -c olevod.cookies -o $TMPJSON
 
-    m3u8=$(jq -r .url $TMPJSON)
-    nid=$(jq -r .nid $TMPJSON)
-    next=$(jq -r .link_next $TMPJSON)
+        if [ $? -ne 0 ]; then
+            echo -e "\033[0;31mdata failed on ep: $nid.\033[0m" >&2
+            exit 1
+        fi
 
-    
+        m3u8=$(jq -r '.video_plays[] | select(.src_site | test("sd.*")) | .play_data' $TMPJSON)
+        local nnid=$((nid + 1))
+        next="https://www.olevod.one/vod/$id/play/ep$nnid"
+
+    else
+        curl "$url" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' -H 'Accept-Language: ja-JP,ja;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-US;q=0.6,en;q=0.5' -H 'Cache-Control: max-age=0'  -H 'Connection: keep-alive' -H 'DNT: 1' -H "Referer: $last_url" -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -c olevod.cookies -o $TMPHTML
+
+        grep -oP 'player_aaaa=\K[^<]+' $TMPHTML > $TMPJSON
+
+        m3u8=$(jq -r .url $TMPJSON)
+        nid=$(jq -r .nid $TMPJSON)
+        next=$(jq -r .link_next $TMPJSON)
+    fi
+
+
     if [[ ! -z $m3u8 ]]; then
 		if [ -z "$download" ]; then
 			# save to playlist
@@ -55,10 +78,10 @@ extract_from() {
 		else
 			# download video
 			echo "download_to $m3u8 $target/$nid.mp4"
-			download_to "$m3u8" "$target/$nid.mp4"
+			download_to "$m3u8" "$target/$nid.mp4" 2
 		fi
     else
-        echo "error"
+	    echo -e "\033[0;31mError! m3u8 URL not found.\033[0m" >&2
         exit 1
     fi
 }
@@ -74,9 +97,7 @@ while [[ ! -z $next ]]; do
 
     extract_from
 
-    #interval=$((RANDOM % 20 + 30))
-    interval=$((RANDOM % 10 + 10))
-    echo "sleep $interval";
+    interval=$(( RANDOM % 10 + 10 ))
+    echo "sleep $interval"
     sleep $interval
 done
-
